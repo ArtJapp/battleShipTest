b03509705a3a07aed3c654fa557fb2bdd622f486
Artem Japparov <maleficaralart@gmail.com>
2018-03-19 13:40:38 +0300

    checker fixed

--- BattleShip_Nova.py
+++ BattleShip_Nova.py
@@ -128,24 +128,24 @@ def player_fire(data):
             })
             game.error = False
         else:
+
+            emit("fired", {
+                'game_id': game_id,
+                'enemy_id': enemy_id,
+                'next_player_id': next_player_id,
+                'is_ship': answer,
+                'coord': {
+                    'x': coord_x,
+                    'y': coord_y
+                }
+            }, room=game_id)
+
             if game.finished:
                 print("the game is finished, winner is ", game.winner)
                 emit("game-finished", {
                     'game_id': game_id,
                     'winner_id': game.winner
                 }, room=game_id)
-            else:
-                emit("fired", {
-                    'game_id': game_id,
-                    'enemy_id': enemy_id,
-                    'next_player_id': next_player_id,
-                    'is_ship': answer,
-                    'coord': {
-                        'x': coord_x,
-                        'y': coord_y
-                    }
-                }, room=game_id)
-
 
     except KeyError:
         emit('error', {
